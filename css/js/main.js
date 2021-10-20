bbff1d5d21ad514e7be7d0ed0dbeb81526db4143
artemisia000 <misiapierobon@gmail.com>
2021-10-20 15:15:20 +0200

    intro js

---
+++ css/js/main.js
@@ -0,0 +1,23 @@
+
+
+//ottenere nome del visitatore
+
+const nome = prompt(`Come ti chiami?`);
+console.log(nome);
+
+//ottenere cognome visitatore
+
+const cognome = prompt(`Qualè il tuo cognome?`);
+console.log(cognome);
+
+//ottenere colore preferito
+
+const colore = prompt(`Qualè il tuo colore preferito?`);
+console.log(colore);
+
+//mostrare risultato
+
+document.getElementById(`dati`).innerHTML = `${nome} ${cognome} ${colore} 21 `;
+
+
+
