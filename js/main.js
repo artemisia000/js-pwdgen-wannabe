73ee1e7814c942decbd78aabf5e48503bf2b3857
artemisia000 <misiapierobon@gmail.com>
2021-10-20 18:51:55 +0200

    sistemata cartella js

--- js/main.js
+++ js/main.js
@@ -7,17 +7,14 @@ console.log(nome);
 
 //ottenere cognome visitatore
 
-const cognome = prompt(`Qualè il tuo cognome?`);
+const cognome = prompt(`Qual è il tuo cognome?`);
 console.log(cognome);
 
 //ottenere colore preferito
 
-const colore = prompt(`Qualè il tuo colore preferito?`);
+const colore = prompt(`Qual è il tuo colore preferito?`);
 console.log(colore);
 
 //mostrare risultato
 
 document.getElementById(`dati`).innerHTML = `${nome}${cognome}${colore}21`;
-
-
-
